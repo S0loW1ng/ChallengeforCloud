1570632f01196c610f41dac9d886aba96ffce45f
Enrique Calderon <cadet.calderon@gmail.com>
2020-04-16 05:21:57 +0100

    Interrupt added, Tomorrow will be the last push

--- Ping.go
+++ Ping.go
@@ -6,6 +6,8 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"os/signal"
+	"syscall"
 	time "time"
 
 	"golang.org/x/net/icmp"
@@ -13,6 +15,8 @@ import (
 )
 
 var defddr = "0.0.0.0"
+var success = 0
+var lost = 0
 
 func Ping(address string, timeLengt int) (*net.IPAddr, time.Duration, error) {
 	connection, err := icmp.ListenPacket("ip4:icmp", defddr) //Listen to all replies
@@ -28,17 +32,14 @@ func Ping(address string, timeLengt int) (*net.IPAddr, time.Duration, error) {
 		return addr, 0, err
 	}
 	// Step 1 create the mesage. Thank you Imas for this useful piece of code.
-	println("Step1")
 	ICMPmessage := icmp.Message{
 		Type: ipv4.ICMPTypeEcho, Code: 0, Body: &icmp.Echo{ID: os.Getpid() & 0xffff, Seq: 1, Data: []byte("")},
 	}
 	//Step 2 bytefy the message.
-	println("Step2")
 
 	binary, err := ICMPmessage.Marshal(nil)
 
 	// Step 3 punch it chewie!
-	println("Step3")
 	startTime := time.Now()
 	bites, err := connection.WriteTo(binary, addr)
 	if err != nil {
@@ -51,7 +52,6 @@ func Ping(address string, timeLengt int) (*net.IPAddr, time.Duration, error) {
 
 	// Step 4 wait for reply ( If there is any )
 
-	println("Step4")
 	replySpace := make([]byte, 1500) // sisze of packet
 	err = connection.SetDeadline(time.Now().Add(time.Duration(timeLengt) * time.Second))
 	// catch if there is an error
@@ -73,16 +73,48 @@ func Ping(address string, timeLengt int) (*net.IPAddr, time.Duration, error) {
 	}
 	if returnMessage.Type == ipv4.ICMPTypeEchoReply {
 		println("END")
+		success++
 		return addr, dur, nil
 	} else {
 		println("END with Error")
-		return addr, -1, fmt.Errorf("ERROR from %v", peer)
+		lost++
+		return addr, 0, fmt.Errorf("ERROR from %v", peer)
 	}
 }
 
 func main() {
-	fmt.Println("Hello world")
-	dst, dur, err := Ping("www.googlsdfsdfsdfsdfsfe.com", 10)
+	var count float64 = 0
+	var totalTime float64 = 0
+	var averagetime float64 = 0
 
-	fmt.Printf("Ping to %s , TTL: %s Error code: %s\n", dst, dur, err)
+	sigs := make(chan os.Signal, 1)
+	done := make(chan bool, 1)
+
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() { // This function awaits for an interrupt to occure
+		sig := <-sigs
+		fmt.Println()
+		fmt.Println(sig)
+		done <- true
+	}()
+	fmt.Println("To exit press CTRL + C")
+
+	go func() { // Runs Infinite loop for ping
+		for {
+			dst, dur, err := Ping("8.8.8.8", 1)
+			fmt.Printf("Ping to %s , TTL: %s Error code: %s\n", dst, dur, err)
+			count = count + 1 // counts howmany packets have been send
+			/*
+				This is chunk of code calculates the average of the trip
+				vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+			*/
+			ms := float64(dur / time.Millisecond)
+			totalTime = totalTime + ms
+			averagetime = float64(totalTime / count)
+		}
+	}()
+	<-done
+	fmt.Printf("Total packets send: %d Lost: %d Recieved: %d Average TTL: %f\n", lost+success, lost, success, averagetime)
+
 }
